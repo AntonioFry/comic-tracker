773f159aeb0c90c590f7b09a588e4c774c53122f
Antonio Fry <antoniofry2000@gmail.com>
2019-10-30 14:51:22 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  componentDidMount() {
+
+  }
+  
+  render() {
+    return (
+      <main>
+
+      </main>
+    );
+  }
+}
+
+export default App;
