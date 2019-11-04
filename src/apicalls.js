747b37098ff061ad5dc574cd73a1dfb5af4dfeb1
Antonio Fry <antoniofry2000@gmail.com>
2019-11-04 10:37:29 -0700

    Remove console.log

--- src/apicalls.js
+++ src/apicalls.js
@@ -3,7 +3,6 @@ import crypto from "crypto";
 
 
 export const getWeeklyComics = async () => {
-  console.log(process.env)
   const ts = new Date().getTime();
   const hash = crypto.createHash('md5').update(ts.toString() + PRIVATE_API_KEY + PUBLIC_API_KEY).digest('hex');
   let url = 'https://gateway.marvel.com:443/v1/public/comics?dateDescriptor=thisWeek&limit=10&apikey=e8c94ee2b9df00711e62ec67be2f7325'
