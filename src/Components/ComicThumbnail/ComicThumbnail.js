375b0108bf42c4ea2fdc076895d97d5b06117549
Antonio Fry <antoniofry2000@gmail.com>
2019-11-06 14:32:49 -0700

    Style thumbnail title to remove overflow

--- src/Components/ComicThumbnail/ComicThumbnail.js
+++ src/Components/ComicThumbnail/ComicThumbnail.js
@@ -5,7 +5,7 @@ export const ComicThumbnail = ({ cover, title }) => {
   return (
     <div className="thumbnail-container">
       <img className="thumbnail-cover" src={`${cover.path}.${cover.extension}`} alt="comic preview cover" />
-      <p className="thumbnail-title">{title}</p>
+      <h3 className="thumbnail-title">{title}</h3>
     </div>
   )
 };
