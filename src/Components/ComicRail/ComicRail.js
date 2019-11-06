aafe3b350c76a0282e5b9123089dbe6592f0deb9
Antonio Fry <antoniofry2000@gmail.com>
2019-11-06 12:03:51 -0700

    Import and Implement ComicThumbnail in ComicRail component

--- src/Components/ComicRail/ComicRail.js
+++ src/Components/ComicRail/ComicRail.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ComicThumbnail } from '../ComicThumbnail/ComicThumbnail';
 
 export class ComicRail extends Component {
   constructor() {
@@ -11,12 +12,12 @@ export class ComicRail extends Component {
   render() {
     const displayedComics = this.props.comics.map(comic => {
       return (
-        
+        <ComicThumbnail id={comic.id} key={comic.id} cover={comic.cover} title={comic.title} />
       )
     });
     return (
       <section>
-
+        {displayedComics}
       </section>
     )
   }
