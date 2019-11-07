bad03d5c4441c50bcc752bc96d528a727131a244
Antonio Fry <antoniofry2000@gmail.com>
2019-11-07 11:41:37 -0700

    Implement Link on comic thumbnail

--- src/Components/ComicThumbnail/ComicThumbnail.js
+++ src/Components/ComicThumbnail/ComicThumbnail.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './ComicThumbnail.css'
+import './ComicThumbnail.css';
+import { Link } from 'react-router-dom';
 
-export const ComicThumbnail = ({ cover, title }) => {
+export const ComicThumbnail = ({ id, cover, title }) => {
   return (
-    <div className="thumbnail-container">
-      <img className="thumbnail-cover" src={`${cover.path}.${cover.extension}`} alt="comic preview cover" />
-      <h3 className="thumbnail-title">{title}</h3>
-    </div>
+    <Link className='comic-link' to={`/${id}`}>
+      <div className="thumbnail-container">
+        <img className="thumbnail-cover" src={`${cover.path}.${cover.extension}`} alt="comic preview cover" />
+        <h3 className="thumbnail-title">{title}</h3>
+      </div>
+    </Link>
   )
 };
