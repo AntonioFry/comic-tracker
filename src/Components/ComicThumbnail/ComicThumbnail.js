030418227a6ffbddefaa61af7e56850bf412e14f
Antonio Fry <antoniofry2000@gmail.com>
2019-11-07 12:34:32 -0700

    Import Route into app

--- src/Components/ComicThumbnail/ComicThumbnail.js
+++ src/Components/ComicThumbnail/ComicThumbnail.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './ComicThumbnail.css';
 import { Link } from 'react-router-dom';
 
-export const ComicThumbnail = ({ id, cover, title }) => {
+export const ComicThumbnail = ({ cover, title }) => {
   return (
-    <Link className='comic-link' to={`/${id}`}>
+    <Link className='comic-link' to={`/comic-details`}>
       <div className="thumbnail-container">
         <img className="thumbnail-cover" src={`${cover.path}.${cover.extension}`} alt="comic preview cover" />
         <h3 className="thumbnail-title">{title}</h3>
