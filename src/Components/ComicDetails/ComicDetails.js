fab47dd9434bfc2f00d1a855d7fdccd33c0a0d20
Antonio Fry <antoniofry2000@gmail.com>
2019-11-11 15:02:06 -0700

    componentDidMount invokes fetch call to grab data on comic issue and set to state

--- src/Components/ComicDetails/ComicDetails.js
+++ src/Components/ComicDetails/ComicDetails.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { getComicIssue } from '../../API/apicalls';
 
 export class ComicDetails extends Component {
   constructor() {
-    super()
+    super();
+    this.state = {
+      data: {},
+    }
+  }
+
+  async componentDidMount() {
+    const { id } = this.props;
+    try {
+      const issueData = await getComicIssue(id);
+      this.setState({ data: issueData });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
