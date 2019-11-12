03300ce401e610391b9d0003c1f8899adecfa2a8
Antonio Fry <antoniofry2000@gmail.com>
2019-11-12 11:56:40 -0700

    Gave all jsx classNames -n comicDetails

--- src/Components/ComicDetails/ComicDetails.js
+++ src/Components/ComicDetails/ComicDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getComicIssue } from '../../API/apicalls';
+import './ComicDetails.css';
 
 export class ComicDetails extends Component {
   constructor() {
@@ -23,18 +24,19 @@ export class ComicDetails extends Component {
     console.log(this.state.data)
     const { cover, title, prices, dates, description, pageCount, issueNumber } = this.state.data;
     return (
-      <section>
+      <section className="comic-details-container">
         <div className="comic-cover-container">
           {cover === undefined ? null : 
             <img className="comic-cover" src={`${cover.path}.${cover.extension}`} alt="Cover of comic issue"/>
           }
         </div>
-        {title === undefined ? null : <article>
-          <h3>{title}</h3>
-          <h3>Price</h3>
-          <p>{prices[0].price}</p>
-          <h3>Page Count</h3>
-          <p>{pageCount}</p>
+        {title === undefined ? null : 
+        <article className="comic-info-container">
+          <h3 className="comic-info-header">{title}</h3>
+          <h3 className="comic-info-header">Price</h3>
+          <p className="comic-info-text">{prices[0].price}</p>
+          <h3 className="comic-info-header">Page Count</h3>
+          <p className="comic-info-text">{pageCount}</p>
         </article>}
       </section>
     )
