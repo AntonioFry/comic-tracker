8f2bf422100f3f40d8c3c027a08b2abb16fb5de5
Antonio Fry <antoniofry2000@gmail.com>
2019-11-25 12:11:21 -0700

    Add link to homepage on marvle logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <header>
-      <img className="marvel-logo" src={require('../../../public/marvel-logo.png')} alt="Marvel comics logo" />
+      <Link to="/">
+        <img className="marvel-logo" src={require('../../../public/marvel-logo.png')} alt="Marvel comics logo" />
+      </Link>
     </header>
   )
 }
