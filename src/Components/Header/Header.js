1b53201436d42864ff4f4887cfd97a4bb9c84850
Antonio Fry <antoniofry2000@gmail.com>
2019-11-25 12:23:21 -0700

    Import NavBar into Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import { NavBar } from '../NavBar/NavBar';
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
@@ -8,6 +9,7 @@ export const Header = () => {
       <Link to="/">
         <img className="marvel-logo" src={require('../../../public/marvel-logo.png')} alt="Marvel comics logo" />
       </Link>
+      <NavBar />
     </header>
   )
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+export const NavBar = () => {
   return (
     <nav>
       <Link to="">Home</Link>
