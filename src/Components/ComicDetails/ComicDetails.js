93444543eb3c67e590e75a576c187d523b4a1253
Antonio Fry <antoniofry2000@gmail.com>
2019-12-10 20:47:18 -0700

    Save buttons render based off saved boolean

--- src/Components/ComicDetails/ComicDetails.js
+++ src/Components/ComicDetails/ComicDetails.js
@@ -21,7 +21,7 @@ export class ComicDetails extends Component {
   }
 
   render() {
-    const { cover, title, prices, dates, description, pageCount, issueNumber, creators } = this.state.data;
+    const { cover, title, prices, dates, description, pageCount, issueNumber, creators, saved } = this.state.data;
     let backgroundImage;
     cover === undefined ? null : backgroundImage = { 
       background: `linear-gradient(rgb(0, 0, 0), rgba(0, 0, 0, 0.5)) 0% 0% / cover no-repeat,
@@ -50,8 +50,8 @@ export class ComicDetails extends Component {
               <h3 className="comic-info-header">Creators</h3>
               {formattedCreators}
             </article>}
-            <button className="save-button">SAVE</button>
-            <button className="save-button">UNSAVE</button>
+            {saved === true ? null : <button className="save-button">SAVE</button>}
+            {saved === false ? null : <button className="save-button">UNSAVE</button>}
           </div>
         </div>
         }
