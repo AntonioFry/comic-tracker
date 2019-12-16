58c7a1adacfae56c0c941d2095d9dc5f370d1508
Antonio Fry <antoniofry2000@gmail.com>
2019-12-16 14:31:40 -0700

    Build out jsx in SavedComics component

--- src/Components/SavedComics/SavedComics.js
+++ src/Components/SavedComics/SavedComics.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { getComicIssue } from '../../API/apicalls';
+import { ComicThumbnail } from '../ComicThumbnail/ComicThumbnail';
 import { connect } from 'react-redux';
 
-export const SavedComics = () => {
+export const SavedComics = ({ savedIds }) => {
+  const savedComicsData = savedIds.map(id => {
+    const comicData = getComicIssue(id)
+    return (<ComicThumbnail
+      id={comicData.id}
+      cover={comicData.cover}
+      title={comicData.title}
+    />) 
+  })
   return (
     <section>
-      
+      {savedComicsData}
     </section>
   )
 }
