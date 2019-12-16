41d96038b247460d0fad8312692e94cd9bbe93f0
Antonio Fry <antoniofry2000@gmail.com>
2019-12-16 14:38:13 -0700

    Update NavBar with SavedComics link

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 export const NavBar = () => {
   return (
     <nav>
-      <Link className="nav-bar-links" to="/">Home</Link>
+      <Link className="nav-bar-links" to="/">HOME</Link>
+      <Link className="nav-bar-links" to="/saved-comics">SAVED</Link>
     </nav>
   )
 }
