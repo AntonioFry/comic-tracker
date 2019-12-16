d87871a0c8c91d483875a048e24cd7fbd5d18728
Antonio Fry <antoniofry2000@gmail.com>
2019-12-16 15:04:06 -0700

    Add conditional rendering for cover in ComicThumbnail

--- src/Components/ComicThumbnail/ComicThumbnail.js
+++ src/Components/ComicThumbnail/ComicThumbnail.js
@@ -6,10 +6,10 @@ import { getComicIssue } from '../../API/apicalls';
 export const ComicThumbnail = ({ id, cover, title }) => {
   return (
     <Link className='comic-link' to={`/${id}`} onClick={() => getComicIssue(id)}>
-      <div className="thumbnail-container">
+      {cover === undefined ? null : <div className="thumbnail-container">
         <img className="thumbnail-cover" src={`${cover.path}.${cover.extension}`} alt="comic preview cover" />
         <h3 className="thumbnail-title">{title}</h3>
-      </div>
+      </div>}
     </Link>
   )
 };
