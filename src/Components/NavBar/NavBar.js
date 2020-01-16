47fed208fc4ce7b38f23b4885cd3fecc7577ef4a
Antonio Fry <antoniofry2000@gmail.com>
2020-01-16 12:33:35 -0700

    Style and alter layout of NavBar component

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 export const NavBar = () => {
   return (
     <nav>
+      <img className='hamburger-icon' src={require('../../../public/hamburger-icon.png')}/>
       <Link className="nav-bar-links" to="/">HOME</Link>
       <Link className="nav-bar-links" to="/saved-comics">SAVED</Link>
     </nav>
