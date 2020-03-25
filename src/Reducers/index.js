b692627056f9eaca873867e9c29808eb7f336333
Antonio Fry <antoniofry2000@gmail.com>
2020-03-25 10:56:52 -0600

    Import and add savedCharcatersReducer to index file

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { comicsReducer } from './comicsReducer';
 import { savedReducer } from './savedReducer';
 import { charactersReducer } from './chractersReducer';
+import { savedCharactersReducer } from './savedCharactersReducer';
 
 export const rootReducer = combineReducers({
   comics: comicsReducer,
   savedIds: savedReducer,
-  characters: charactersReducer
+  characters: charactersReducer,
+  savedCharacters: savedCharactersReducer
 })
