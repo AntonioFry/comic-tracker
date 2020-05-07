9b86c2adf4613a2e1d83207993f0ec4509bc4ce2
Antonio Fry <antoniofry2000@gmail.com>
2020-05-07 14:32:31 -0600

    Tested that App matches snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -2,21 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import "@testing-library/jest-dom/extend-expect";
+
+import renderer from 'react-test-renderer';
 
 
 describe('App', () => {
-
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    const characters = [];
-    const comics = { 
-      "Weekly Comics": [
-      ], 
+  let comics;
+  let characters;
+  
+  beforeEach(() => {
+    characters = [
+      {
+        name: "Spider-Man",
+        id: 1009610,
+        desciption: "Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider. Adopting the name Spider-Man, Peter hoped to start a career using his new abilities. Taught that with great power comes great responsibility, Spidey has vowed to use his powers to help people."
+      }
+    ];
+    comics = {
+      "Weekly Comics": [],
       "Spider-Man Comics": [
-        { 
-          title: "Marvel Age Spider-Man Vol. 2: Everyday Hero (Digest)", 
-          id: 1308, 
-          cover: { path: "http://i.annihil.us/u/prod/marvel/i/mg/9/20/4bc665483c3aa", extension: "jpg" } 
+        {
+          title: "Marvel Age Spider-Man Vol. 2: Everyday Hero (Digest)",
+          id: 1308,
+          cover: { path: "http://i.annihil.us/u/prod/marvel/i/mg/9/20/4bc665483c3aa", extension: "jpg" }
         },
         {
           title: "Amazing Spider-Man (1999) #558 (Turner Variant)",
@@ -25,7 +34,11 @@ describe('App', () => {
         }
       ]
     };
-    
+  })
+
+  it('Renders without crashing', () => {
+    const div = document.createElement('div');
+  
     ReactDOM.render(
     <BrowserRouter>
       <App characters={characters} comics={comics} />
@@ -35,5 +48,13 @@ describe('App', () => {
   });
 
 
+  it('Matches snapshot', () => {
+    const tree = renderer.create(
+      <BrowserRouter>
+        <App characters={characters} comics={comics} />
+      </BrowserRouter>
+   ).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 
 })
