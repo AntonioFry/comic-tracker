13e2bca0136f7b5bf0c69c01de87aad09bfc20eb
Antonio Fry <antoniofry2000@gmail.com>
2020-11-01 14:50:13 -0700

    Setup test file

---
+++ src/Components/Characters/CharacterCard/CharacterCard.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
+import '@testing-library/jest-dom';
+
+describe('CharacterCard', () => {
+  let props = {
+    thumbnail: 'Spider-Man.JPG',
+    id: '1100616',
+    name: 'Spider-Man'
+  };
+
+  
+});
